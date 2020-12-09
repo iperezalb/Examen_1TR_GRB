4317cc4b8c7012bc72df233cfe21c1b83adea31b
Iris <72754372+iperezalb@users.noreply.github.com>
2020-12-09 18:59:17 +0100

    Creacion elementos
    He intentado crear las esferas de manera aleatoria, pero no consigo que quede similar al video

---
+++ Perez_Albaladejo_Iris/Assets/Scripts/Creador_enemigos.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creador_enemigos : MonoBehaviour
+{
+    [SerializeField] GameObject Enemigo;
+
+    [SerializeField] Transform initPos;
+
+    Vector3 newPosY;
+    Vector3 newPosX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int n = 0; n < 20; n++)
+        {
+            float randomPosY = Random.Range(0f, 20f);
+
+            float randomPosX = Random.Range(-10f, 10f);
+
+            Vector3 desplazarPosY = new Vector3(0, randomPosY, 2 * n);
+
+            newPosY = initPos.position + desplazarPosY;
+
+            Instantiate(Enemigo, newPosY, Quaternion.identity);
+
+            Vector3 desplazarPosX = new Vector3(randomPosX, 0, 2 * n);
+
+            newPosX = initPos.position + desplazarPosX;
+
+
+            Instantiate(Enemigo, newPosX, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
