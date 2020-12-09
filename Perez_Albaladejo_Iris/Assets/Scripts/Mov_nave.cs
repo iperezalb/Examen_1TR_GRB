fe0e0de74e499a5a25bbeeb3262cf77e4e35a5aa
Iris <72754372+iperezalb@users.noreply.github.com>
2020-12-09 18:09:28 +0100

    Movimiento nave
    He intentado crear todos los movimientos que hace la nave

---
+++ Perez_Albaladejo_Iris/Assets/Scripts/Mov_nave.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mov_nave : MonoBehaviour
+{
+    [SerializeField] float velocidad = 5f;
+
+    private float desplZ;
+    private float desplX;
+    private float desplY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        desplX = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.left * desplX * Time.deltaTime * velocidad);
+
+        desplY = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.back * desplZ * Time.deltaTime * velocidad);
+
+
+    }
+}
